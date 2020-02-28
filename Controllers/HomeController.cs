a60af2b26becdc11da7a1c673c8af04551d7de35
akhileshsh026 <akhileshsh026@hotmail.com>
2020-02-28 22:35:39 +0530

    the emperory information is show in the List and getAll method is accecible via tha /api/Home/GetAll

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPI_All_Sesions.Models;
 
 namespace WebAPI_All_Sesions.Controllers
 {
     public class HomeController : ApiController
     {
-              
+        static List<Employee> empList = new List<Employee>
+        {
+            new Employee {Id=1,Name="Akhilesh",Location="Mandla",salary=77864 },
+            new Employee {Id=2,Name="Akhilesh1",Location="Mandla1",salary=87864 },
+            new Employee {Id=3,Name="Akhilesh2",Location="Mandla2",salary=97864 },
+            new Employee {Id=4,Name="Akhilesh3",Location="Mandla3",salary=177864 },
+        };
+
+              public List<Employee> GetAll()
+        {
+            return empList;
+
+        }
     }
 }
