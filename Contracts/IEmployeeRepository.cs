d0bced26769b0e5337e2ead6452a93d22f1322cb
Akhileshsh028 <Akhileshsh028@outlook.com>
2020-03-04 22:33:06 +0530

    All the funcanality is divided into different modules

---
+++ Contracts/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI_All_Sesions.Models;
+
+namespace WebAPI_All_Sesions.Contracts
+{
+    public interface IEmployeeRepository
+    {
+        List<Employee> GetAll();
+
+        Employee GetEmployeeById(int id);
+        void CreateEmployee(Employee emp);
+        void UpdateEmployee(Employee emp);
+        void DeleteEmployee(int id);
+    }
+}
---
+++ Models/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI_All_Sesions.Contracts;
+
+namespace WebAPI_All_Sesions.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        static List<Employee> empList = new List<Employee>
+        {
+            new Employee {Id=1,Name="Akhilesh",Location="Mandla",salary=77864 },
+            new Employee {Id=2,Name="Akhilesh1",Location="Mandla1",salary=87864 },
+            new Employee {Id=3,Name="Akhilesh2",Location="Mandla2",salary=97864 },
+            new Employee {Id=4,Name="Akhilesh3",Location="Mandla3",salary=177864 },
+        };
+
+        public void CreateEmployee(Employee emp)
+        {
+            empList.Add(emp);
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            empList.Remove(empList.FirstOrDefault(e => e.Id == id));
+        }
+
+        public List<Employee> GetAll()
+        {
+            return empList;
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            return empList.FirstOrDefault(e => e.Id == id);
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            Employee TempEmp = empList.FirstOrDefault(e => e.Id == emp.Id);
+            TempEmp.Name = emp.Name;
+            TempEmp.Location = emp.Location;
+            TempEmp.salary = emp.salary;
+          
+        }
+    }
+}
