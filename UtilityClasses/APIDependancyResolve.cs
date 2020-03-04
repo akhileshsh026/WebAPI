2a43e7b34f014b4c29c8c224e93965317e79c15c
Akhileshsh028 <Akhileshsh028@outlook.com>
2020-03-04 23:03:12 +0530

    Straing of the dependancy Injection by injecting the parametrised contsructor of the controller , then created the APIDependancy resolver class which impliments the IdependancyResolver interface , then by using Unity we implimented this constructor

---
+++ UtilityClasses/APIDependancyResolve.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+using Unity;
+
+namespace WebAPI_All_Sesions.UtilityClasses
+{
+    public class APIDependancyResolve : IDependencyResolver
+    {
+        UnityContainer 
+        public IDependencyScope BeginScope()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
